7dbebca80a16556764486fec11a3ba7e33db5a63
Nicolae Bacila <nicolae.bacila@infiniswiss.com>
2020-10-03 12:24:34 +0300

    Add snow flake rendering

--- src/lib/fight_for_life.rs
+++ src/lib/fight_for_life.rs
@@ -1,10 +1,12 @@
 use amethyst::{GameData, SimpleState, StateData};
+use amethyst::assets::{AssetStorage, Handle, Loader};
 use amethyst::core::shred::World;
 use amethyst::core::Transform;
 use amethyst::prelude::*;
-use amethyst::renderer::Camera;
+use amethyst::renderer::{Camera, ImageFormat, SpriteSheet, SpriteSheetFormat, Texture, SpriteRender};
+use amethyst::core::math::Vector3;
 
-use crate::lib::snow_flake::SNOW_FLAKE_WIDTH;
+use crate::lib::snow_flake::{SNOW_FLAKE_WIDTH, SNOW_FLAKE_HEIGHT};
 use crate::lib::SnowFlake;
 
 pub const ARENA_HEIGHT: f32 = 100.0;
@@ -17,9 +19,11 @@ pub struct FightForLife;
 impl SimpleState for FightForLife {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
+        let sprite_sheet_handle = load_sprite_sheet(world);
 
         world.register::<SnowFlake>();
-        initialize_snow_flake(world);
+
+        initialize_snow_flake(world, sprite_sheet_handle);
         initialize_camera(world);
     }
 }
@@ -37,15 +41,44 @@ fn initialize_camera(world: &mut World) -> () {
         .build();
 }
 
-pub fn initialize_snow_flake(world: &mut World) -> () {
+fn initialize_snow_flake(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) -> () {
     let mut transform = Transform::default();
 
-    let y = ARENA_HEIGHT / 2.0;
-    transform.set_translation_xyz(SNOW_FLAKE_WIDTH * 0.5, y, 0.0);
+    let x: f32 = SNOW_FLAKE_WIDTH;
+    let y: f32 = SNOW_FLAKE_HEIGHT;
+    transform.set_translation_xyz(x, y, 0.0);
+    transform.set_scale(Vector3::new(0.1, 0.1, 0.1));
+    let sprite_reader = SpriteRender::new(sprite_sheet_handle, 0);
 
     world
         .create_entity()
+        .with(sprite_reader.clone())
         .with(SnowFlake::new())
         .with(transform)
         .build();
+}
+
+fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
+    // Load the sprite sheet necessary to render the graphics.
+    // The texture is the pixel data
+    // `texture_handle` is a cloneable reference to the texture
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "sprites/snow_flake.jpg",
+            ImageFormat::default(),
+            (),
+            &texture_storage,
+        )
+    };
+
+    let loader = world.read_resource::<Loader>();
+    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+    return loader.load(
+        "sprites/snow_flake.ron",
+        SpriteSheetFormat(texture_handle),
+        (),
+        &sprite_sheet_store,
+    );
 }
